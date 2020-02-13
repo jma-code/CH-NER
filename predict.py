4e0962e78ed0feab2f6308033027cb03b24f4d59
jty <1170719210@qq.com>
2020-02-13 16:16:51 +0800

    数据预处理

--- predict.py
+++ predict.py
@@ -14,18 +14,14 @@ config.gpu_options.allow_growth = True
 config.gpu_options.per_process_gpu_memory_fraction = 0.3
 params = cf.ConfigPredict('predict', 'config/params.conf')
 params.load_config()
-embedding_mat = np.random.uniform(-0.25, 0.25, (4756, 300))  # 4756*300
-embedding_mat = np.float32(embedding_mat)
-embeddings = embedding_mat
-num_tags = len(data_process.tag2label)
-summary_path = "logs"
+
 '''
 预测引擎
 '''
 
 
 # 输入句子id和保存好的模型参数进行预测，输出标签id
-def predict_one_batch(ses, seqs):
+def predict_one_batch(model, ses, seqs):
     """
 
     :param ses:
@@ -47,7 +43,7 @@ def predict_one_batch(ses, seqs):
 
 
 # 输入句子，得到预测标签id，并转化为label
-def demo_one(ses, sent, batch_size, vocab, tag_label, shuffle):
+def demo_one(model, ses, sent, batch_size, vocab, shuffle):
     """
 
     :param ses:
@@ -62,10 +58,10 @@ def demo_one(ses, sent, batch_size, vocab, tag_label, shuffle):
     # batch_yield就是把输入的句子每个字的id返回，以及每个标签转化为对应的tag2label的值
     label_list = []
     for seqs, labels in train_utils.batch_yield(sent, batch_size, vocab, tag2label, shuffle):
-        label_list_, _ = predict_one_batch(ses, seqs)
+        label_list_, _ = predict_one_batch(model, ses, seqs)
         label_list.extend(label_list_)
     label2tag = {}
-    for tag, label in tag_label.items():
+    for tag, label in tag2label.items():
         label2tag[label] = tag if label != 0 else label
     tag = [label2tag[label] for label in label_list[0]]
     return tag
@@ -155,11 +151,9 @@ def get_ORG_entity(tag_seq, char_seq):
             continue
     return ORG
 
-def predict(model_path, embeddings, update_embedding, hidden_dim, num_tags, clip, summary_path, optimizer):
-    ckpt_file = tf.train.latest_checkpoint(model_path)
+def predict(model, batch_size, vocab, shuffle):
+    ckpt_file = tf.train.latest_checkpoint(params.model_path)
     print(ckpt_file)
-    model = BiLSTM_CRF(embeddings, update_embedding, hidden_dim, num_tags, clip, summary_path, optimizer)
-    model.build_graph()
     saver = tf.train.Saver()
     with tf.Session(config=config) as sess:
         print('============= demo =============')
@@ -173,13 +167,22 @@ def predict(model_path, embeddings, update_embedding, hidden_dim, num_tags, clip
             else:
                 demo_sent = list(demo_sent.strip())
                 demo_data = [(demo_sent, ['O'] * len(demo_sent))]
-                tag = model.demo_one(sess, demo_data)
+                tag = demo_one(model, sess, demo_data, batch_size, vocab, shuffle)
                 PER, LOC, ORG = get_entity(tag, demo_sent)
                 print('PER: {}\nLOC: {}\nORG: {}'.format(PER, LOC, ORG))
 
 if __name__ == '__main__':
-    predict(embeddings, params.update_embedding, params.hidden_dim, num_tags, params.clip, summary_path,
+    embedding_mat = np.random.uniform(-0.25, 0.25, (4756, 300))  # 4756*300
+    embedding_mat = np.float32(embedding_mat)
+    embeddings = embedding_mat
+    num_tags = len(data_process.tag2label)
+    summary_path = "logs"
+    model = BiLSTM_CRF(embeddings, params.update_embedding, params.hidden_dim, num_tags, params.clip, summary_path,
                        params.optimizer)
+    model.build_graph()
+
+
+    predict(params.model, 64, 'data/word2id', False)
     '''
     input_sent = ['小', '明', '的', '大', '学', '在', '北', '京', '的', '北', '京', '大', '学']
     get_sent = [(input_sent, ['O'] * len(input_sent))]
