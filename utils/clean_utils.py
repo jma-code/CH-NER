ca43c450c19a4534d1028b5a52692a378d598f73
jma-code <sdumajie@163.com>
2020-02-27 14:54:49 +0800

    添加清洗工具类

---
+++ utils/clean_utils.py
@@ -0,0 +1,21 @@
+class Clean(object):
+    def __init__(self):
+        pass
+
+    def cut_corpus(self):
+        pass
+
+    def deal_paddle(self):
+        pass
+
+    def rule_set(self):
+        pass
+
+    def item_eval(self):
+        pass
+
+class Clean_64_Corpus(Clean):
+    pass
+
+class Clean_Punc_Corpus(Clean):
+    pass
