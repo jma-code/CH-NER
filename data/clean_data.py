ffb64b0e91abb78a5ccbe9ad994aeae29249b8b6
jma-code <sdumajie@163.com>
2020-02-27 13:03:32 +0800

    数据清洗

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,5 +1,6 @@
 import re
 
+#清洗数据
 def clean_data(data_path, data_with_label, data_without_label):
     """
     :param data_path:原文件路径
@@ -16,6 +17,7 @@ def clean_data(data_path, data_with_label, data_without_label):
         for line in lines:
             if line != '\n':
                 char_, tag_ = line.strip().split()
+                #去除括号
                 if char_ not in ['（', '）']:
                     char += char_
                     tag = tag + tag_ + ' '
@@ -46,6 +48,7 @@ def clean_data(data_path, data_with_label, data_without_label):
 
     fwn.close()
 
+#清洗百度LAC标注
 def clean_lac(lac_data, lac_clean_data):
     """
     :param lac_data: 百度数据集
@@ -92,6 +95,7 @@ def clean_lac(lac_data, lac_clean_data):
             fw.write(sent_ + '|' + label_ + '\n')
     fw.close()
 
+#抽取实体
 def extract(path, tag_label):
     """
     :param path:百度标注数据集路径
@@ -128,84 +132,61 @@ def extract(path, tag_label):
                 result_data.append('None')
     return result_data, num
 
+#抽取实体，评估
 def comput_eval(paddle_path, test_path):
     """
-    :param paddle_path:百度标注文件路径
-    :param test_path: 括测试标注文件路径
+    计算各种测评值
+    :param paddle_path: paddle输出实体的路径
+    :param test_path: 原始测试数据集的路径
     :return:
     """
-    tag_label = 'LOC'
-    paddle_data, num_loc_paddle = extract(paddle_path, tag_label)
-    test_data, num_loc_test = extract(test_path, tag_label)
-    correct_loc_num = 0
-    for i in range(len(test_data)):
-        if test_data[i] != 'None':
-            for j in range(len(test_data[i])):
-                for k in range(len(paddle_data[i])):
-                    if test_data[i][j] == paddle_data[i][k]:
-                        correct_loc_num += 1
-                        break
-    print('LOC正确识别的实体数：', correct_loc_num)
-    print('LOC总的识别实体数：', num_loc_paddle)
-    print('LOC的正确率accurary:', float(correct_loc_num) / float(num_loc_paddle))
-    print('LOC的召回率recall：', float(correct_loc_num) / float(num_loc_test))
-    med_add = (float(correct_loc_num) / float(num_loc_paddle)) + (float(correct_loc_num) / float(num_loc_test))
-    f_loc = 2 * float(correct_loc_num) / float(num_loc_test) * float(correct_loc_num) / float(num_loc_paddle)/med_add
-    print('LOC的F测度值：', f_loc)
+    tag_labels = ['LOC', 'PER', 'ORG']
+    total_items_paddle = 0
+    total_items_test = 0
+    for tag_label in tag_labels:
+        paddle_data, num_paddle = extract(paddle_path, tag_label)
+        test_data, num_test = extract(test_path, tag_label)
+        correct_num = 0
+        for i in range(len(test_data)):
+            if test_data[i] != 'None':
+                if tag_label == 'LOC':
+                    print(test_data[i], paddle_data[i], i + 1)
+                for j in range(len(test_data[i])):
+                    for k in range(len(paddle_data[i])):
+                        if test_data[i][j] == paddle_data[i][k]:
+                            correct_num += 1
+                            break
+        print(tag_label + '正确识别的实体数：', correct_num)
+        print(tag_label + '总的识别实体数：', num_paddle)
+        print(tag_label + '的正确率accuracy:', float(correct_num) / float(num_paddle))
+        print(tag_label + '的召回率recall：', float(correct_num) / float(num_test))
+        med_add = (float(correct_num) / float(num_paddle)) + (float(correct_num) / float(num_test))
+        f_loc = 2 * float(correct_num) / float(num_test) * float(correct_num) / float(num_paddle) / med_add
+        print(tag_label + '的F测度值：', f_loc)
 
-    tag_label = 'PER'
-    paddle_data, num_per_paddle = extract(paddle_path, tag_label)
-    test_data, num_per_test = extract(test_path, tag_label)
-    correct_per_num = 0
-    for i in range(len(test_data)):
-        if test_data[i] != 'None':
-            for j in range(len(test_data[i])):
-                for k in range(len(paddle_data[i])):
-                    if test_data[i][j] == paddle_data[i][k]:
-                        correct_per_num += 1
-                        break
-    print('PER正确识别的实体数：', correct_per_num)
-    print('PER总的识别实体数：', num_per_paddle)
-    print('PER的正确率accurary:', float(correct_per_num) / float(num_per_paddle))
-    print('PER的召回率recall：', float(correct_per_num) / float(num_per_test))
-    med_add = (float(correct_per_num) / float(num_per_paddle)) + (float(correct_per_num) / float(num_per_test))
-    f_per = 2 * float(correct_per_num) / float(num_per_test) * float(correct_per_num) / float(num_per_paddle) / med_add
-    print('PER的F测度值：', f_per)
+        total_items_paddle += num_paddle
+        total_items_test += num_test
+    print('总的识别实体数：', total_items_paddle)
+    print('总的实体数量：', total_items_test)
 
-    tag_label = 'ORG'
-    paddle_data, num_org_paddle = extract(paddle_path, tag_label)
-    test_data, num_org_test = extract(test_path, tag_label)
-    correct_org_num = 0
-    for i in range(len(test_data)):
-        if test_data[i] != 'None':
-            print(test_data[i], paddle_data[i], i + 1)
-            for j in range(len(test_data[i])):
-                for k in range(len(paddle_data[i])):
-                    if test_data[i][j] == paddle_data[i][k]:
-                        correct_org_num += 1
-                        break
-    print('ORG正确识别的实体数：', correct_org_num)
-    print('ORG总的识别实体数：', num_org_paddle)
-    print('ORG的正确率accurary:', float(correct_org_num) / float(num_org_paddle))
-    print('ORG的召回率recall：', float(correct_org_num) / float(num_org_test))
-    med_add = (float(correct_org_num) / float(num_org_paddle)) + (float(correct_org_num) / float(num_org_test))
-    f_org = 2 * float(correct_org_num) / float(num_org_test) * float(correct_org_num) / float(num_org_paddle) / med_add
-    print('ORG的F测度值：', f_org)
-
-    total_item_paddle = num_loc_paddle + num_org_paddle + num_per_paddle
-    total_item_test = num_loc_test + num_org_test + num_per_test
-    print('总的识别实体数：', total_item_paddle)
-    print('总的实体数量：',total_item_test)
-
-
+#修改标注
 def match_label(paddle_path, test_path, test_save_path):
+    """
+    :param paddle_path:百度数据集
+    :param test_path: 测试数据集
+    :param test_save_path: 保存结果
+    :return:
+    """
     label_t = []
     sent_t = []
+    t = 0
+    labels = ['B-PER', 'I-PER', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC', 'O']
     with open(paddle_path, encoding = 'utf-8') as f:
         with open(test_path, encoding = 'utf-8') as fr:
             lines_p = f.readlines()
             lines_t = fr.readlines()
             for line_p,line_t in zip(lines_p,lines_t):
+                t = t + 1
                 tag = ''
                 data_p = []
                 data_t = []
@@ -214,28 +195,22 @@ def match_label(paddle_path, test_path, test_save_path):
                 data_p.append(label_.strip().split())
                 data_t.append(label.strip().split())
                 for i in range(len(data_p[0])):
-                    if data_p[0][i] == 'B-PER' and data_t[0][i] == 'O':
-                        tag = tag + 'B-PER' + ' '
-                    elif data_p[0][i] == 'I-PER' and data_t[0][i] == 'O':
-                        tag = tag + 'I-PER' + ' '
-                    elif data_p[0][i] == 'I-PER' and data_t[0][i] == 'B-PER':
-                        tag = tag + 'I-PER' + ' '
-                    elif data_p[0][i] == 'B-ORG' and data_t[0][i] == 'O':
-                        tag = tag + 'B-ORG' + ' '
-                    elif data_p[0][i] == 'I-ORG' and data_t[0][i] == 'O':
-                        tag = tag + 'I-ORG' + ' '
-                    elif data_p[0][i] == 'I-ORG' and data_t[0][i] == 'B-ORG':
-                        tag = tag + 'I-ORG' + ' '
-                    elif data_p[0][i] == 'B-LOC' and data_t[0][i] == 'O':
-                        tag = tag + 'B-LOC' + ' '
-                    elif data_p[0][i] == 'I-LOC' and data_t[0][i] == 'O':
-                        tag = tag + 'I-LOC' + ' '
-                    elif data_p[0][i] == 'I-LOC' and data_t[0][i] == 'B-LOC':
-                        tag = tag + 'I-LOC' + ' '
-                    elif data_p[0][i] == 'B-ORG' and data_t[0][i] == 'B-LOC':
-                        tag = tag + 'B-ORG' + ' '
-                    elif data_p[0][i] == 'I-ORG' and data_t[0][i] == 'I-LOC':
-                        tag = tag + 'I-ORG' + ' '
+                    if data_p[0][i] in labels[:6] and data_t[0][i] == 'O':
+                        tag = tag + data_p[0][i] + ' '
+                    elif data_p[0][i] == labels[1] and data_t[0][i] == labels[0]:
+                        tag = tag + labels[1] + ' '
+                    elif data_p[0][i] == labels[3] and data_t[0][i] == labels[2]:
+                        tag = tag + labels[3] + ' '
+                    elif data_p[0][i] == labels[5] and data_t[0][i] == labels[4]:
+                        tag = tag + labels[5] + ' '
+                    elif data_p[0][i] == labels[2] and data_t[0][i] in [labels[0], labels[4]]:
+                        tag = tag + labels[2] + ' '
+                    elif data_p[0][i] == labels[3] and data_t[0][i] in [labels[1], labels[5]]:
+                        tag = tag + labels[3] + ' '
+                    elif data_p[0][i] == labels[4] and data_t[0][i] in [labels[0], labels[2]]:
+                        tag = tag + labels[4] + ' '
+                    elif data_p[0][i] == labels[5] and data_t[0][i] in [labels[1], labels[3]]:
+                        tag = tag + labels[5] + ' '
                     else:
                         tag = tag + data_t[0][i] + ' '
                 label_t.append(tag)
@@ -246,10 +221,36 @@ def match_label(paddle_path, test_path, test_save_path):
             fw.write(sent_ + '|'+label_ + '\n')
     fw.close()
 
+#修改标注形式，改成单字标注
+def sent2word(test_path, test_save_path):
+    """
+    :param test_path:原文件路径
+    :param test_save_path: 存储路径
+    :return:
+    """
+    words = []
+    tags = []
+    with open(test_path, encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            data = []
+            sent_, label_ = line.strip().split('|')
+            data.append(label_.strip().split())
+            for i in range(len(data[0])):
+                words.append(sent_[i])
+                tags.append(data[0][i])
+
+    with open(test_save_path, 'w', encoding='utf-8') as fw:
+        for word, tag in zip(words, tags):
+            fw.write(word + ' ' + tag + '\n')
+            if word in ['。', '！']:
+                fw.write('\n')
+    fw.close()
 
 
 if __name__ == '__main__':
     #clean_data('test_data', 'msra_label.tsv', 'infer.tsv')
     #clean_lac('lac_data_msra.txt', 'lac_data_msra_clean.tsv')
-    #match_label('lac_data_msra_clean.tsv', 'msra_label.tsv', 'msra_label_sec.tsv')
-    comput_eval('lac_data_msra_clean.tsv', 'msra_label_sec.tsv')
+    #match_label('lac_data_clean.tsv', 'test_label.tsv', 'test_label_thr.tsv')
+    #comput_eval('lac_data_clean.tsv', 'test_label_thr.tsv')
+    sent2word('test_label_thr.tsv', 'wiki_data')
